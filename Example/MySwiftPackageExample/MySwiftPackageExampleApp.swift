80ca1f8007cfbfa4ff7919d0a9c73deb3b7ab30a
Mikio Nakata <21nakatam@gmail.com>
2023-08-31 16:47:43 +0900

    ✨ Add example project

---
+++ Example/MySwiftPackageExample/MySwiftPackageExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftPackageExampleApp.swift
+//  MySwiftPackageExample
+//
+//  Created by Mikio Nakata on 2023/08/31.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftPackageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
