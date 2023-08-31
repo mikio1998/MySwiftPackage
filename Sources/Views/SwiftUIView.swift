40b28ce48ada4140dbe6ba65536027c2a212e2d3
Mikio Nakata <21nakatam@gmail.com>
2023-08-31 17:54:36 +0900

    ✨ Add size modifiers to image

--- Sources/Views/SwiftUIView.swift
+++ Sources/Views/SwiftUIView.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Mikio Nakata on 2023/08/31.
 //
+// ** When you have resources (ie image asset) added to swift package, and you want to utilize those resources..
+// https://useyourloaf.com/blog/add-resources-to-swift-packages/
 
 import Foundation
 import SwiftUI
@@ -14,9 +16,10 @@ public struct SwiftUIView: View {
     public init() {}
     public var body: some View {
         Text("THIS IS THE SWIFTUI VIEW FROM THE PACKAGE!")
-//        Image("Planet")
-//        Image(uiimage: UIImage(named: "", in: .module, with: nil))
-        Image("Planet", bundle: .module)
+        Image("Planet", bundle: .module) // **
+            .resizable()
+            .frame(width: 100, height: 100, alignment: .center)
+            .aspectRatio(contentMode: .fit)
     }
 }
 
